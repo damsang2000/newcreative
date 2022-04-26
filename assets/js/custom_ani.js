94f55c934dbec8eef1756cd6f572d12ceeec3ccd
sang <sang@gmail.com>
2022-04-26 11:34:02 +0700

    update 35

--- assets/js/custom_ani.js
+++ assets/js/custom_ani.js
@@ -1,5 +1,6 @@
 const photo = document.querySelector(".Photography");
 const categories = document.querySelector("#photo");
+console.log(categories)
 categories.addEventListener("onclick", function () {
-  console.log("hello");
+  alert("hello");
 });
